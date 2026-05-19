fc10297dcddf78ca77bade3a80488844051e1c73
Le Thi Hai <lehai.lita6@gmail.com>
2026-05-19 11:15:00 +0700

    feat: tich hop bo loc tim kiem nang cao va sap xep gia san pham

--- scr/WebBanTraSua/BLL/SanPhamBLL.cs
+++ scr/WebBanTraSua/BLL/SanPhamBLL.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 using WebBanTraSua.DAL;
 
 namespace WebBanTraSua.BLL
@@ -12,9 +12,41 @@ namespace WebBanTraSua.BLL
             return dal.LayDanhSachSanPham();
         }
 
+        public DataTable LaySanPhamTheoLoai(int maLoai)
+        {
+            return dal.LaySanPhamTheoLoai(maLoai);
+        }
+
         public DataTable LaySanPhamBanChay(int top = 3)
         {
             return dal.LaySanPhamBanChay(top);
+        }
+
+        public DataTable LaySanPhamNangCao(int? maLoai, string search, string sortPrice)
+        {
+            return dal.LaySanPhamNangCao(maLoai, search, sortPrice);
+        }
+
+        // --- ADMIN METHODS ---
+
+        public DataTable LayDanhSachSanPhamTatCa()
+        {
+            return dal.LayDanhSachSanPhamTatCa();
+        }
+
+        public bool ThemSanPham(DTO.SanPhamDTO sp)
+        {
+            return dal.ThemSanPham(sp);
+        }
+
+        public bool CapNhatSanPham(DTO.SanPhamDTO sp)
+        {
+            return dal.CapNhatSanPham(sp);
+        }
+
+        public bool XoaSanPham(int id)
+        {
+            return dal.XoaSanPham(id);
         }
     }
 }
