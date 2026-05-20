5fe0df98b5e9db3d158ad15a9e9d05010d53a9bc
Ngo Thi Ngoc Quyen <ngothingocquyen.thtanthanha.lp@soctrang.edu.vn>
2026-05-20 15:30:00 +0700

    ui: nang cap trang thai het hang truc quan va hieu ung hover thuc don

--- scr/WebBanTraSua/SanPham.aspx.cs
+++ scr/WebBanTraSua/SanPham.aspx.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Data;
+using System;
+using System.Web.UI.WebControls;
 using WebBanTraSua.BLL;
 
 namespace WebBanTraSua
@@ -7,24 +7,50 @@ namespace WebBanTraSua
     public partial class SanPham : System.Web.UI.Page
     {
         SanPhamBLL sanPhamBLL = new SanPhamBLL();
+        LoaiSPBLL loaiSPBLL = new LoaiSPBLL();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
+                LoadDanhMuc();
                 LoadSanPham();
             }
         }
 
+        private void LoadDanhMuc()
+        {
+            rptLoaiSP.DataSource = loaiSPBLL.LayDanhSachLoai();
+            rptLoaiSP.DataBind();
+        }
+
         private void LoadSanPham()
         {
-            DataTable dt = sanPhamBLL.LayDanhSachSanPham();
-            
-            if (dt.Rows.Count > 0)
+            int? catId = null;
+            if (Request.QueryString["catId"] != null)
             {
-                rptSanPham.DataSource = dt;
-                rptSanPham.DataBind();
+                int id;
+                if (int.TryParse(Request.QueryString["catId"], out id))
+                {
+                    catId = id;
+                }
             }
+
+            string search = txtSearch.Text.Trim();
+            string sort = ddlSortPrice.SelectedValue;
+
+            rptSanPham.DataSource = sanPhamBLL.LaySanPhamNangCao(catId, search, sort);
+            rptSanPham.DataBind();
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            LoadSanPham();
+        }
+
+        protected void ddlSortPrice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadSanPham();
         }
     }
 }
